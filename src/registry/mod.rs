2087dcb59bd9d1815e45a9188d73c761ea884176
Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>
2023-02-17 19:45:54 +0100

    Remove unused code
    Signed-off-by: Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>

--- src/registry/mod.rs
+++ src/registry/mod.rs
@@ -4,34 +4,3 @@
 //! UR registry.
 
 pub mod passport;
-
-use core::num::NonZeroU32;
-use ur::registry::crypto_hdkey::{
-    BaseHDKey, CoinType, CryptoCoinInfo, CryptoKeypath, DerivedKey, PathComponent,
-};
-
-pub fn casa_export<C>(
-    public_key: [u8; 33],
-    chain_code: [u8; 32],
-    fingerprint: NonZeroU32,
-) -> BaseHDKey<'static, C>
-where
-    C: ur::collections::Vec<PathComponent>,
-{
-    let use_info = CryptoCoinInfo::new(CoinType::BTC, 0);
-    let origin = CryptoKeypath::new_master(fingerprint);
-
-    let derived_key = DerivedKey {
-        is_private: false,
-        key_data: public_key,
-        chain_code: Some(chain_code),
-        use_info: Some(use_info),
-        origin: Some(origin),
-        children: None,
-        parent_fingerprint: None,
-        name: None,
-        note: None,
-    };
-
-    BaseHDKey::DerivedKey(derived_key)
-}
